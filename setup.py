5015b9fda28b959b57e4478815e234788c182d00
Anirudh Raman <anirudh.raman.24@ucl.ac.uk>
2026-02-06 16:27:51 +0000

    restructuring

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="im2mesh",
+    name="im2sim",
     version="0.1.0",
     packages=find_packages(),  # auto-finds your Python packages
 )

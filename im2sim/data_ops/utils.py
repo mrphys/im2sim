7404dcb7893fb54e9b85b49e83b1673e7faf5cfc
Anirudh Raman <anirudh.raman.24@ucl.ac.uk>
2026-02-06 17:43:32 +0000

    added data utils

---
+++ im2sim/data_ops/utils.py
@@ -0,0 +1,31 @@
+import numpy as np
+import torch
+from itertools import combinations
+from torch_geometric.utils import to_undirected
+
+
+def get_node_ids(mesh):
+    cell_ids = np.zeros((len(mesh.points),))
+    for id in np.unique(mesh['CellEntityIds']):
+        cells = mesh.extract_cells(np.where(mesh['CellEntityIds'] == id)[0])['vtkOriginalPointIds']
+        nodes = np.unique(cells)
+        cell_ids[nodes] = id
+    return cell_ids
+
+def get_edges_tet(mesh):
+    tet_cells = mesh.extract_cells(np.where(mesh['CellEntityIds'] == 0)[0])
+    tet_cells = tet_cells.cells.reshape(-1, 5)[:, 1:]
+    edges = np.reshape(np.array([list(combinations(cell,2)) for cell in tet_cells]), [-1,2])
+    edges = torch.from_numpy(np.unique(edges, axis=0).T)
+    edges = to_undirected(edges)
+    return edges
+
+def get_node_features(mesh, feature_names):
+    features = torch.from_numpy(np.array([mesh.point_data[name] for name in feature_names]).T)
+    return features
+
+def set_structure_masks(data, mesh, structure_list):
+    node_ids = get_node_ids(mesh)
+    for id, structure in enumerate(structure_list):
+            setattr(data, f"is_{structure}", torch.from_numpy(node_ids==id))
+    return data

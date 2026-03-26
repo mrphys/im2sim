044a0223bd7f5662790b86de4a2a6815bd4ee4b5
Anirudh Raman <anirudh.raman.24@ucl.ac.uk>
2026-03-26 17:30:09 +0000

    KnnMSE edits

--- im2sim/losses/feature.py
+++ im2sim/losses/feature.py
@@ -22,16 +22,15 @@ class KnnMSE(torch.nn.Module):
     def forward(self, true_graph, pred_graph):
         c1 = true_graph.x[:,:3]
         c2 = pred_graph.x[:,:3]
-        print(c1.shape, c2.shape, c1.max(), c2.max(), c1.min(), c2.min())
+        
         f1 = true_graph.x[:,3:]
         f2 = pred_graph.x[:,3:]
-        print(f1.shape, f2.shape, f1.max(), f2.max(), f1.min(), f2.min())
+
         b1 = true_graph.batch
         b2 = pred_graph.batch
 
 
         f1_interp = knn_interpolate(f1, c1, c2, b1, b2, k=self.k)
-        print(f1_interp.shape, f2.shape, f1_interp.max(), f2.max(), f1_interp.min(), f2.min())
 
         return mse(f1_interp, f2)
         

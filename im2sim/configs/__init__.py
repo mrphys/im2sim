9cbec2584a4883603b8dd4a9961a27997e3f276d
Anirudh Raman <anirudh.raman.24@ucl.ac.uk>
2026-08-21 13:25:35 +0100

    added LayerConfig to configs init

--- im2sim/configs/__init__.py
+++ im2sim/configs/__init__.py
@@ -2,10 +2,12 @@ from im2sim.configs.halfunet import HalfUNetConfig
 from im2sim.configs.imageconv import ImageConvBlockConfig
 from im2sim.configs.reverse_halfunet import ReverseHalfUNetConfig
 from im2sim.configs.unet import UNetConfig
+from im2sim.configs.core import LayerConfig
 
 __all__ = [
     "HalfUNetConfig",
     "UNetConfig",
     "ReverseHalfUNetConfig",
     "ImageConvBlockConfig",
+    "LayerConfig"
 ]
